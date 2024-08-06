b98bbf6f40c22468c6758217feead35737e383b2
jc <46619361+juancwu@users.noreply.github.com>
2024-08-06 10:57:21 -0400

    working on parser

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,1 +1,111 @@
-pub struct Lexer {}
+use crate::compiler::token::Token;
+
+#[derive(Debug, Clone)]
+pub struct Lexer {
+    current_character: char,
+    current_string: String,
+    current_position: usize,
+    leading_position: usize,
+    input_string: String,
+    available_bytes: i64,
+}
+
+impl Lexer {
+    pub fn new(input_string: String) -> Self {
+        Self {
+            current_character: '\0',
+            current_string: String::new(),
+            current_position: 0,
+            leading_position: 0,
+            input_string,
+            available_bytes: 0,
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        if self.is_eof() {
+            return Token::EOF;
+        }
+
+        self.skip_whitespace();
+
+        let mut token: Token = Token::EOF;
+
+        while !self.is_eof() {
+            match self.current_character {
+                '"' => {
+                    // skip initial double quotes
+                    let mut escape = false;
+                    while !self.is_eof() && self.read_character().is_some() {
+                        if escape {
+                            self.current_string.push(self.current_character);
+                            escape = false;
+                            continue;
+                        }
+                        if self.current_character == '\\' {
+                            escape = true;
+                            continue;
+                        }
+                        if self.current_character == '"' {
+                            break;
+                        }
+                        self.current_string.push(self.current_character);
+                    }
+                    token = Token::RawValue(self.current_string.clone())
+                }
+                '{' => {
+                    token = Token::CurlyLeft;
+                }
+                '}' => {
+                    token = Token::CurlyRight;
+                }
+                ':' => {
+                    token = Token::DoubleColon;
+                }
+                _ => {
+                    token = Token::Illegal;
+                }
+            }
+        }
+
+        self.current_string.clear();
+
+        token
+    }
+
+    /// Reads a raw string from the input source.
+    fn read_raw_string(&mut self, include_linefeed: bool) {}
+
+    fn read_character(&mut self) -> Option<char> {
+        if self.is_eof() {
+            return None;
+        }
+
+        self.current_character = match self.input_string.chars().nth(self.leading_position) {
+            Some(ch) => ch,
+            None => '\0',
+        };
+
+        self.current_position = self.leading_position;
+        self.leading_position += 1;
+
+        Some(self.current_character)
+    }
+
+    fn peek_character(&self) -> Option<char> {
+        if self.is_eof() {
+            return None;
+        }
+        self.input_string.chars().nth(self.leading_position)
+    }
+
+    fn is_eof(&self) -> bool {
+        self.leading_position >= self.input_string.len()
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.current_character == ' ' {
+            self.read_character();
+        }
+    }
+}
--- src/compiler/token.rs
+++ src/compiler/token.rs
@@ -1,7 +1,49 @@
+#[derive(Debug, Clone)]
 pub enum Token {
     /// Represents illegal characters that shouldn't be in the .hermes syntax
     Illegal,
-    MetadataBlock,
-    RouteBlock,
-    HeadersBlock,
+    EOF,
+    /// This is a data block and it can be a key block that is reserved, or it is custom defined.
+    /// A block is defined by the (type, identier) where the identifier can be an empty string for
+    /// annonynous block.
+    ///
+    /// Available reserved blocks:
+    /// metadata
+    /// request
+    /// container
+    /// folder
+    /// headers
+    /// variables
+    /// environment
+    /// body
+    /// queries
+    Block(String, String),
+    /// Typical identifier in any language. This will mostly just be
+    /// block names that are used to reference to defined blocks or for reserved keywords.
+    ///
+    /// Keep in mind that identifier keywords only appear at the beginning of any line in a block.
+    ///
+    /// Available identifier keywords:
+    /// type - type of hermes file, usually defined in a metadata block
+    /// name - the type of a collection, request, or folder
+    /// text - text type of multipart form data field
+    /// file - file type of multipart form data field
+    /// environment - use an enviroment
+    /// add - add a single request
+    /// include - include all requests from a given path
+    Identifier(String),
+    /// The SelectorOperator refers to the two colons "::".
+    SelectorOperator,
+    /// Refers to any raw value read from a hermes file. For example, the JSON body string would be
+    /// a raw value, as well as the value of a query parameter.
+    RawValue(String),
+    /// Some blocks such as headers, queries, form-urlencoded, and mutipart-form can have enabled
+    /// fields which are included in the request.
+    StateEnabled,
+    /// Some blocks such as headers, queries, form-urlencoded, and mutipart-form can have disabled
+    /// fields which are included in the request.
+    StateDisabled,
+    CurlyLeft,
+    CurlyRight,
+    DoubleColon,
 }
